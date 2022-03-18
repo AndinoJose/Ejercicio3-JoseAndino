92d72123dea33d4bb52cae9e5f8d79e366404281
AndinoJose <98792132+AndinoJose@users.noreply.github.com>
2022-03-18 17:51:50 -0600

    Add files via upload

---
+++ BaseDatos/Accesos/Coneccion.cs
@@ -0,0 +1,93 @@
+﻿using BaseDatos.Entidades;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseDatos.Accesos
+{
+    public class Coneccion
+    {
+        readonly string cadena = "Server = Localhost; Port=3306; Database= ejercicio3; Uid=root; pwd=1234;";
+
+        MySqlConnection conn;
+        MySqlCommand cmd;
+
+
+        public Usuarios Login(string idusuario, string clave)
+        {
+            Usuarios user = null;
+
+            try
+            {
+                string sql = "SELECT * FROM usuarios  WHERE IdUsuario= @IdUsuario AND Clave=@Clave ";
+
+                conn = new MySqlConnection(cadena);
+                conn.Open();
+
+                cmd = new MySqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@IdUsuario", idusuario);
+                cmd.Parameters.AddWithValue("@Clave", clave);
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    user = new Usuarios();
+                    user.IdUsuario = reader[0].ToString();
+                    user.Nombre = reader[1].ToString();
+                    user.Apellido = reader[2].ToString();
+                    user.Edad = Convert.ToInt32((reader[3].ToString()));
+                    user.Clave = reader[4].ToString();
+
+                }
+                reader.Close();
+                conn.Close();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return user;
+        }
+
+        public DataTable MostrarUsuarios ()
+        {
+            DataTable MostrarUsuariosDT = new DataTable();
+
+            try
+            {
+                string sql = "SELECT * FROM usuarios;";
+
+                conn = new MySqlConnection(cadena);
+                conn.Open();
+
+                cmd = new MySqlCommand(sql, conn);
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                MostrarUsuariosDT.Load(reader);
+
+
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return MostrarUsuariosDT;
+
+        }
+
+
+
+
+    }
+}
+    
---
+++ BaseDatos/Entidades/Usuarios.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseDatos.Entidades
+{
+    public class Usuarios
+    {
+        public String IdUsuario { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Edad { get; set; }
+        public string Clave { get; set; }
+
+        public Usuarios()
+        {
+      
+        }
+
+        public Usuarios(string idUsuario, string nombre, string apellido, int edad, string clave)
+        {
+            IdUsuario = idUsuario;
+            Nombre = nombre;
+            Apellido = apellido;
+            Edad = edad;
+            Clave = clave;
+        }
+    }
+}
